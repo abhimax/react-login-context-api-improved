6fdc4bcde672f657f9cd8b9235d3a47921bbfc25
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-09-16 05:58:29 +0530

    feat: add local storage value to handle login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+  useEffect(()=>{
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+    if(storedUserLoggedInInformation === '1'){
+      setIsLoggedIn(true);
+    }
+  },[]);
+
+  const loginHandler = (email, password) => {
+    localStorage.setItem('isLoggedIn','1');
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem('isLoggedIn');
+  };
+
   return (
-    <div className="App">
-      <p>Login Test</p>
-    </div>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
 }
 

f884037f678e9d309aa78beb2c9d5a4240070745
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-09-23 09:07:28 +0530

    fetch: improvement auto suggestion

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,3 +1,6 @@
 import React from "react";
-const AuthContext = React.createContext({ isLoggedIn : false });
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+});
 export default AuthContext;
